42cb9890428dedea73f18d49225a3ca8f0386d02
Prakash Raj <prakashraj@apptino.com>
2025-09-29 13:40:09 +0530

    cart single item ui folder

---
+++ src/app/[locale]/(app)/cart/components/CartItemCard/CartItemCard.tsx
@@ -0,0 +1,16 @@
+// // {
+//     "brandName": "Generic",
+//     "brandProductId": "adsfasd",
+//         "hsnCode": "22011010",
+//     "minOrderQuantity": 0,
+//     "packagingQty": "1",
+// imge:null,
+// "dexeiption:" ""
+
+// }
+
+function CartItemCard() {
+  return <div>CartItemCard</div>;
+}
+
+export default CartItemCard;
--- src/app/[locale]/(app)/page.tsx
+++ src/app/[locale]/(app)/page.tsx
@@ -1,9 +1,11 @@
 import AddMoreProducts from "@/components/Global/Products/AddMoreProducts";
+import CartItemCard from "./cart/components/CartItemCard/CartItemCard";
 
 export default function Home() {
   return (
     <>
       <AddMoreProducts />
+      <CartItemCard />
     </>
   );
 }
