67bd928e3717270428ac6816ef990a4ed4733ff4
Ajitha2122 <ajitha@apptino.com>
2025-11-07 15:39:33 +0530

    new changes

--- src/components/custom/index.ts
+++ src/components/custom/index.ts
@@ -1,6 +1,6 @@
 // Dashboard Toolbar Components
-export { DashboardToolbar } from "./dashboard-toolbar";
 export { ActionToolbar, pluralize } from "./action-toolbar";
+export { DashboardToolbar } from "./dashboard-toolbar";
 
 // Search Components
 export { default as SearchBox } from "./search";
@@ -10,21 +10,21 @@ export { LoadingButton } from "./loading-button";
 export { SaveCancelToolbar } from "./save-cancel-toolbar";
 
 // Global Components
-export { GlobalLoaderWrapper } from "./global-loader-wrapper";
+
 
 // Other Components
 export { default as Logo } from "./logo";
 
 // Type exports
 export type {
-  DashboardToolbarProps,
-  DashboardToolbarRef,
-} from "@/types/dashboard-toolbar";
-export type {
   ActionToolbarProps,
   ActionToolbarRef,
   SelectionCount
 } from "@/types/action-toolbar";
+export type {
+  DashboardToolbarProps,
+  DashboardToolbarRef
+} from "@/types/dashboard-toolbar";
 export type { SaveCancelToolbarProps } from "@/types/save-cancel";
 export type {
   SectionToolbarProps,
