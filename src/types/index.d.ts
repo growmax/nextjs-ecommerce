28436918b45f3ae60b8bd573beda38cc7c010907
Prakash Raj <prakashraj@apptino.com>
2025-11-12 14:16:49 +0530

    TypeScript doesn't recognize the .png import
    Declaration provided

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,34 @@
 // Global type declarations
+
+// Image imports
+declare module "*.png" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: import("next/image").StaticImageData;
+  export default content;
+}
+
 export {};
